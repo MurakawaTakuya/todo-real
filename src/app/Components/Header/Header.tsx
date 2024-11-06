87c9728778ab828392ca2dd84aa419208de543af
MurakawaTakuya <murakawatakuya5@gmail.com>
2024-11-06 19:56:52 +0900

    初期状態からスマホサイズになるように変更

--- src/app/Components/Header/Header.tsx
+++ src/app/Components/Header/Header.tsx
@@ -1,9 +1,5 @@
 import styles from "./Header.module.scss";
 
 export default function Header() {
-  return (
-    <header className={styles.header}>
-      <h1>Todo Real(仮)</h1>
-    </header>
-  );
+  return <header className={styles.header}>Todo Real(仮)</header>;
 }

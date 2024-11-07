cda549129be787b46ddcc03030c36a680bc79e1e
MurakawaTakuya <murakawatakuya5@gmail.com>
2024-11-07 12:41:21 +0900

    resolve parsing error

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -15,7 +15,7 @@ module.exports = {
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: ["tsconfig.json"],
+    project: ["tsconfig.json", "tsconfig.functions.json"],
     sourceType: "module",
   },
   ignorePatterns: [

2ef9abfad41a81be39c386e5ba7624ebdb3ffd9d
Morimoto Mana <cnj14140@ict.nitech.ac.jp>
2024-11-11 17:28:45 +0900

    create user form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Posts from "./Components/Posts/Posts";
+import UserForm from "./Components/UserForm/UserForm";
 
 export default function Home() {
   return (
     <>
       <Posts />
+      <UserForm />
     </>
   );
 }

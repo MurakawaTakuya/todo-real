47bd68147c17a2c7dd032ee4955409748f46758a
Takumi Ito <cnj19005@ict.nitech.ac.jp>
2024-11-13 16:00:10 +0900

    目標を作成し、期限の日付を設定する

---
+++ src/app/Components/Goal/Goal.tsx
@@ -0,0 +1,64 @@
+"use client";
+import { useState } from "react";
+
+export default function Goal() {
+  const [text, setText] = useState("");
+  const [dueDate, setDueDate] = useState("");
+
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+
+    try {
+      const response = await fetch(
+        "http://127.0.0.1:5001/todo-real-c28fa/asia-northeast1/firestore/goal/",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            userId: "sampleUser123",
+            text: text,
+            deadline: new Date(dueDate).toISOString(),
+          }),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error(`Network response was not ok: ${response.statusText}`);
+      }
+
+      const data = await response.json();
+      console.log("Success:", data);
+
+      setText("");
+      setDueDate("");
+    } catch (error: any) {
+      console.error("Fetch error:", error.message);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        目標テキスト:
+        <input
+          type="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          required
+        />
+      </label>
+      <label>
+        期日:
+        <input
+          type="datetime-local"
+          value={dueDate}
+          onChange={(e) => setDueDate(e.target.value)}
+          required
+        />
+      </label>
+      <button type="submit">アップロード</button>
+    </form>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Goal from "./Components/Goal/Goal";
 import Posts from "./Components/Posts/Posts";
 
 export default function Home() {
   return (
     <>
       <Posts />
+      <Goal></Goal>
     </>
   );
 }

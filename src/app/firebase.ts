9fc3017795019062f72dfbe9835d6acbd332108e
MurakawaTakuya <murakawatakuya5@gmail.com>
2024-11-21 15:32:23 +0900

    ブラウザを閉じてもログイン状態を維持

--- src/app/firebase.ts
+++ src/app/firebase.ts
@@ -2,9 +2,12 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import {
+  browserLocalPersistence,
   connectAuthEmulator,
   getAuth,
   GoogleAuthProvider,
+  onAuthStateChanged,
+  setPersistence,
 } from "firebase/auth";
 import { connectStorageEmulator, getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -29,6 +32,41 @@ export const storage = getStorage(app);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
 
+// ブラウザを閉じてもログイン状態を維持
+setPersistence(auth, browserLocalPersistence)
+  .then(() => {
+    console.log("Persistence set to local.");
+  })
+  .catch((error) => {
+    console.error("Error setting persistence:", error);
+  });
+
+// ログイン状態を監視
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    const isGoogle = user.providerData.some(
+      (profile) => profile.providerId === "google.com"
+    );
+    const isEmail = user.providerData.some(
+      (profile) => profile.providerId === "password"
+    );
+    const isGuest = user.isAnonymous;
+
+    if (isGoogle) {
+      console.log("Googleアカウントでログインしています");
+    } else if (isEmail) {
+      console.log("メールとパスワードでログインしています");
+    } else if (isGuest) {
+      console.log("匿名（ゲスト）でログインしています");
+    } else {
+      console.log("その他の方法でログインしています");
+    }
+  } else {
+    console.log("No user is logged in.", user);
+  }
+});
+
+// エミュレータの設定
 if (process.env.NEXT_PUBLIC_USE_FIREBASE_EMULATOR === "true") {
   connectStorageEmulator(storage, "localhost", 9199);
   connectAuthEmulator(auth, "http://127.0.0.1:9099");

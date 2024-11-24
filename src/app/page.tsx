f352ea9fe0af32986cd9829318e9d5c0aba079d4
MurakawaTakuya <murakawatakuya5@gmail.com>
2024-11-24 19:57:33 +0900

    Cloud Messagingを実装

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
+"use client";
+import "@/utils/getNotification";
+import "@/utils/messaging";
+import requestPermission from "@/utils/requestNotificationPermission";
+import { useEffect } from "react";
 import Goal from "./Components/Goal/Goal";
 import ImageUploader from "./Components/PostForm/PostForm";
 import Posts from "./Components/Posts/Posts";
 import UserForm from "./Components/UserForm/UserForm";
 
 export default function Top() {
+  useEffect(() => {
+    requestPermission();
+  }, []);
   return (
     <>
       <Posts />
